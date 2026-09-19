3f4072e02fceca8e1e9d43756b336365cc6af2be
shehryar-ahmed44172 <ahmedshehryar645@gmail.com>
2026-09-19 14:07:59 +0500

    fix(call): import kIsWeb

--- lib/services/call/call_service.dart
+++ lib/services/call/call_service.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io' show Platform;
 
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
